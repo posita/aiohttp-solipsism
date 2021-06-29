f9fbb37eaa671566d0ceb0571bca71de59bb7d82
Matt Bogosian <matt@bogosian.net>
2021-06-29 18:44:58 -0500

    Requested modifications
    See https://github.com/aio-libs/aiohttp/issues/5834#issuecomment-870794096.
    
    * Add https://github.com/bmerry/async-solipsism/tree/fix-create-server-explicit-socket as submodule
    * Rename `event_loop` to `loop`
    * Add `pytest-mock` dependency
    * Patch `aiohttp.test_utils.get_port_socket` for test
    * `await` explicit client creation

--- tests/test_so_happy_together.py
+++ tests/test_so_happy_together.py
@@ -4,23 +4,20 @@ from aiohttp import web
 
 
 @pytest.fixture
-def event_loop():
-    loop = async_solipsism.EventLoop()
-    yield loop
-    loop.close()
-
-
-@pytest.fixture
-def fake_client(
-    event_loop,  # noqa: F811 # pylint: disable=redefined-outer-name
-    aiohttp_client,
-):
-    app = web.Application()
-    return event_loop.run_until_complete(aiohttp_client(app))
+def loop(mocker):
+    mocker.patch(
+        "aiohttp.test_utils.get_port_socket",
+        lambda host, port: async_solipsism.ListenSocket((host, port)),
+    )
+    as_loop = async_solipsism.EventLoop()
+    yield as_loop
+    as_loop.close()
 
 
 async def test_integration(
-    fake_client,  # noqa: F811 # pylint: disable=redefined-outer-name
+    aiohttp_client,  # noqa: F811 # pylint: disable=redefined-outer-name
 ):
-    resp = await fake_client.post("/hey", json={})
+    app = web.Application()
+    client = await aiohttp_client(app, server_kwargs={"port": 80})
+    resp = await client.post("/hey", json={})
     assert resp.status == 404
